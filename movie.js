d8d076f3947d177504badae8b6779452d4088527
Ashutosh Krish <122876636+ashutoshkri@users.noreply.github.com>
2023-12-24 08:00:47 -0800

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,44 @@
+let APIKey = "6e93d49a";
+let searchInput = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchBtn");
+
+const getData = async(movie) => {
+    try {
+        let fetchData = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${APIKey}&t=${movie}`);
+        let jsonData = await fetchData.json();
+
+        console.log(jsonData.Search);
+        // console.log(jsonData.Search);
+        document.querySelector(".card").innerHTML = ""
+        searchInput.value = "";
+
+        let div = document.createElement("div");
+        div.classList.add("movieCard");
+        div.innerHTML = `
+    <img src=${jsonData.Poster} alt="">
+    <div class="cardText">
+        <h1>${jsonData.Title}</h1>
+        <p class="rating">Rating : <span>${jsonData.Ratings[0].Value}</span></p>
+        <a href=""><span>${jsonData.Genre}</span></a>
+        <p>Release : <span>${jsonData.Released}</span></p>
+        <p>Duration : <span>${jsonData.Runtime}</span></p>
+        <p>Discription : <span>${jsonData.Plot}</span></p>
+        <p><span></span></p>
+    </div>
+    `
+
+        document.querySelector(".card").appendChild(div);
+
+    } catch (error) {
+        document.querySelector(".card").innerHTML = "<h1>Enter valid Movie Name</h1>"
+    }
+}
+
+searchBtn.addEventListener("click", function() {
+    let movieName = searchInput.value;
+    if (movieName != "") {
+        getData(movieName);
+    } else {
+        document.querySelector(".card").innerHTML = "<h1>First Search Movie Name</h1>"
+    }
+})
